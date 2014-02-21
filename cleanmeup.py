045bf8a06aa06e25f2c5c54c0e7123858941365f
Jack Wearden <jack@jackwearden.co.uk>
2014-02-21 22:25:06 +0000

    Refactor to a class

--- cleanmeup.py
+++ cleanmeup.py
@@ -10,25 +10,38 @@ change the implementation, not the behaviour.
 Submit your solution in a Pull Request.
 
 """
-import random
-name = raw_input("What is your name? :: ")
-print("Ok, hello there " + name)
-possible_numbers = []
-for i in range(3):
-    possible_numbers.append(random.randint(0, 1000))
-while len(possible_numbers) > 0:
-    secret_number = possible_numbers.pop()
-    number_guessed_yet = False
-    tries = 0
-    print("starting a new round")
-    while not number_guessed_yet:
-        guess = raw_input(name + ", guess what the number might be :: ")
-        guess = int(guess)
-        if guess > secret_number:
-            print("No, that number is too big")
-        elif guess < secret_number:
-            print("No, that number is too small")
-        elif guess == secret_number:
-            print("That's right, the number is " + str(guess))
-            number_guessed_yet = True
-print("bye bye")
+class GuessingGame:
+    import random
+    def __init__(self, rounds, read=input, write=print, source=random.random):
+        self.rounds = rounds
+        self.read   = read
+        self.write  = write
+
+        self.name   = self.read("What is your name? :: ")
+        self.write("Ok, hello there " + self.name)
+        self.secrets = (int(source() * 100) for _ in range(self.rounds))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        secret = next(self.secrets)
+        self.write("starting a new round")
+        guess = secret + 1
+        while not int(guess) == secret:
+            guess = self.read(self.name + ", guess what the number might be :: ")
+            self.write(self.guess_clue(secret, int(guess)))
+
+    def guess_clue(self, secret, guess):
+        def compare_guess(secret, guess):
+            return (guess > secret) - (guess < secret)
+
+        return {
+            1: "No, that number is too big",
+            0: "That's right, the number is " + str(guess),
+           -1: "No, that number is too small"
+        }[compare_guess(secret, guess)]
+
+if __name__ == "__main__":
+  list(GuessingGame(2))
+  print("bye bye")
